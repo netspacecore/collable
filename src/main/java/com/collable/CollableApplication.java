ee6a9719b6c4d736ffc4e16b696702a3a24e5123
Rendani Netshia <netshiarendani@gmail.com>
2022-06-04 23:05:43 +0200

    adding devprint function

--- src/main/java/com/collable/CollableApplication.java
+++ src/main/java/com/collable/CollableApplication.java
@@ -2,6 +2,7 @@ package com.collable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /** Netshia Rendani 
 */
 @SpringBootApplication
@@ -11,4 +12,7 @@ public class CollableApplication {
 		SpringApplication.run(CollableApplication.class, args);
 	}
 
+	public string devprint() {
+		System.out.print(value);
+	}
 }
