146e11ef4c01c1484835d116899e1eb3e271269d
Rendani Netshia <netshiarendani@gmail.com>
2022-06-04 18:51:35 +0200

    Task Name initail project

---
+++ src/main/java/com/collable/CollableApplication.java
@@ -0,0 +1,13 @@
+package com.collable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collable/CollableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
