2c565185f8e6510bf17f4a72cd941538068e86fc
Rendani Netshia <netshiarendani@gmail.com>
2022-06-04 22:52:57 +0200

    Updating Collable Application
    testing

--- src/main/java/com/collable/CollableApplication.java
+++ src/main/java/com/collable/CollableApplication.java
@@ -3,6 +3,7 @@ package com.collable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//rendani netshia trying
 @SpringBootApplication
 public class CollableApplication {
 
